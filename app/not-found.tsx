0aa0a0ee97c71611f6e9f00f09ee9321fedc7e60
김태준 <whiteme0561@naver.com>
2026-01-15 01:51:45 +0900

    feat: 404 not-found 페이지 추가

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <div className="flex items-center justify-center align-middle h-full">
+      <p className="text-sm text-gray-500">
+        요청하신 페이지를 찾을 수 없습니다.
+      </p>
+    </div>
+  );
+}
